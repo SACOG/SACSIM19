6c717b7d3c5d228528cd11737a1eeb8a6445e880
djconly85 <dconly@sacog.org>
2021-01-04 13:32:39 -0800

    commit on 1.4.2021

---
+++ Integrated Land Use Transportation (ILUT)/Python/ILUT/README.txt
@@ -0,0 +1,5 @@
+ILUT python package:
+
+PRIMARY FUNCTIONS:
+-load model run tables into SQL server
+-make combined, parcel-level ILUT summary
---
+++ Integrated Land Use Transportation (ILUT)/Python/ILUT/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+from .ILUT2SQL_bcp import ILUT2SQL_bcp
+from .MakeCombinedILUT import MakeCombinedILUT
+from .bcp_loader import bcp_loader
