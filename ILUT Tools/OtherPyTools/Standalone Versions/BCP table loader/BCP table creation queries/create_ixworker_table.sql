6212f24fa97e59dc4dbbcf3b31b00fc2c02e6038
djconly85 <dconly@sacog.org>
2021-02-18 13:58:35 -0800

    various renamings and reorg

--- ILUT Tools/OtherPyTools/Standalone Versions/BCP table loader/BCP table creation queries/create_ixworker_table.sql
+++
@@ -1,9 +0,0 @@
-
-
---NOTE: this table in CSV form has no headers on it
-
-CREATE TABLE {} (
-	TAZ SMALLINT NULL,
-	IXWkrFraxn REAL NULL, --share of workers who live in region and work outside region
-	extWkrfraxn REAL NULL, --share of workers who live outside region and work in region
-	)
