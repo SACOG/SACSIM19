9404082fe5e3f112c5e371a46c4ca18c5f92cc02
djconly85 <dconly@sacog.org>
2021-12-13 14:31:17 -0800

    working script to pull dataframe of avg speed, epoch count, and standard error of speed for all 9 sacsim time periods.

--- model_network/speed_calibration/time_prd_speeds/cong_speed_model_prds.sql
+++ model_network/speed_calibration/time_prd_speeds/cong_speed_model_prds.sql
@@ -1,26 +1,16 @@
 /*
 Name: cong_speed_model_prds.sql
 Purpose: calculate congested speeds for 9 SACSIM time periods.
+	This script meant to be called from python script, so many parameters are just placeholders.
            
 Author: Darren Conly
-Last Updated: <date>
+Last Updated: Dec 2021
 Updated by: <name>
 Copyright:   (c) SACOG
 SQL Flavor: SQL Server
 */
 
-
-USE NPMRDS
-GO
-
-
---list of weekdays
-DECLARE @weekdays TABLE (day_name VARCHAR(9))
-	INSERT INTO @weekdays VALUES ('Monday')
-	INSERT INTO @weekdays VALUES ('Tuesday')
-	INSERT INTO @weekdays VALUES ('Wednesday')
-	INSERT INTO @weekdays VALUES ('Thursday')
-	INSERT INTO @weekdays VALUES ('Friday')
+SET NOCOUNT ON
 
 
 SELECT
@@ -28,18 +18,19 @@ SELECT
 	measurement_tstamp,
 	speed
 INTO #speeds_prd
-FROM npmrds_2016_alltmc_paxtruck_comb tt
-WHERE DATEPART(mm, tt.measurement_tstamp) IN ('3', '4', '5', '10')
-	AND DATENAME(dw, tt.measurement_tstamp) IN (SELECT day_name FROM @weekdays)
-	AND DATEPART(hh, tt.measurement_tstamp) IN (7)
+FROM {0} tt --speed data table
+WHERE DATEPART(mm, tt.measurement_tstamp) IN ('3', '4', '5', '10') -- months of Mar, Apr, May, Oct
+	AND DATENAME(dw, tt.measurement_tstamp) IN ('Tuesday', 'Wednesday', 'Thursday') -- SACSIM's day of week assumption
+	AND DATEPART(hh, tt.measurement_tstamp) IN ({1}) --list of hours in each sacsim time period
 
 SELECT
 	tmc.tmc,
-	COUNT(*) / SUM(1.0/sp.speed) AS avspd_h07,
-	COUNT(*) AS epcnt_h07,
-	STDEV(sp.speed) AS spd_stdev_h07,
-	STDEV(sp.speed) / (COUNT(*) / SUM(1.0/sp.speed)) AS spd_stderr_h07
-FROM npmrds_2020_alltmc_txt tmc
+	COUNT(*) / SUM(1.0/sp.speed) AS avspd_{2}, --tag for sacsim period (e.g. h07 for 7am, md5 for midday 5-hour period, etc.)
+	SUM(CASE WHEN sp.tmc_code IS NULL THEN 0
+		ELSE 1 END)
+		AS epcnt_{2},
+	STDEV(sp.speed) / (COUNT(*) / SUM(1.0/sp.speed)) AS spd_stderr_{2}
+FROM {3} tmc -- TMC table
 	LEFT JOIN #speeds_prd sp
 		ON tmc.tmc = sp.tmc_code
 --WHERE tmc.tmc = '105-16680'
